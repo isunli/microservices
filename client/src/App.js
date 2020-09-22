e7e889d10fdf40780b0bfa0f0a12950300385ff4
Li Sun <li@5820k-980ti.localdomain>
2020-09-22 11:29:10 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PostCreate from "./PostCreate";
+function App() {
+  return (
+    <div className="container">
+      <PostCreate />
+    </div>
+  );
+}
+
+export default App;
